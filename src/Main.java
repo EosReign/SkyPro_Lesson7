e99a7763719124c746a78e31f056afcbf048c9a1
EosReign <107928418+EosReign@users.noreply.github.com>
2022-08-30 14:19:57 +0300

    ,

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Za Warudo!");
+        //=============FirstTask=============
+        System.out.println("=============FirstTask=============");
+        String firstName = "Ivanov";
+        String middleName = "Ivan";
+        String lastName = "Ivanovich";
+        String fullName = firstName + " " + middleName + " " + lastName;
+        System.out.println("ФИО сотрудника — " + fullName);
+        //=============SecondTask=============
+        System.out.println("=============SecondTask=============");
+        System.out.println("Данные ФИО сотрудника для заполнения отчета — " + fullName.toUpperCase());
+        //=============ThirdTask=============
+        System.out.println("=============ThirdTask=============");
+        String fullName2 = "Иванов Семён Семёнович";
+        fullName2 = fullName2.replace('ё', 'е');
+        fullName2 = fullName2.replace('Ё', 'Е');
+        System.out.println("Данные ФИО сотрудника — " + fullName2);
     }
 }
